1e2c93a2e837d5b503e4e6ddb48838681429a5e7
Ayush <ayushkarma.dev@gmail.com>
2025-12-18 21:06:29 +0530

    Update styling in the scan resume section

--- Frontend/src/components/modules/StartResumeScan.tsx
+++ Frontend/src/components/modules/StartResumeScan.tsx
@@ -4,8 +4,8 @@ function StartResumeScan() {
   return (
     <>
       <div className="StartResumeScan relative flex flex-col items-center justify-center p-10">
-        <div className="mx-auto flex flex-col items-center justify-center text-center">
-          <h3 className="text-bg-gray-1 mt-4 text-left text-lg font-medium md:text-3xl lg:text-4xl">
+        <div className="flex flex-col items-center justify-center text-center">
+          <h3 className="text-bg-gray-1 mt-4 text-xl font-medium md:text-3xl lg:text-4xl">
             Test Your Resume Against ATS Systems
           </h3>
           <p className="text-gray-3 mt-5 mb-8 text-[.9rem] sm:text-[1rem] md:w-3/4 md:text-[1.1rem] lg:text-[1.3rem]">
