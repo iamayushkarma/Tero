aefe448e0bac757e30bfc4a200e9c01117552059
Ayush <ayushkarma.dev@gmail.com>
2025-12-22 22:46:58 +0530

    Fixed styling in Start scan resume section

--- Frontend/src/components/modules/StartResumeScan.tsx
+++ Frontend/src/components/modules/StartResumeScan.tsx
@@ -8,10 +8,10 @@ function StartResumeScan() {
           <h3 className="text-bg-gray-1 mt-4 text-xl font-medium md:text-3xl lg:text-4xl">
             Test Your Resume Against ATS Systems
           </h3>
-          <p className="text-gray-3 mt-5 mb-8 text-[.9rem] sm:text-[1rem] md:w-3/4 md:text-[1.1rem] lg:text-[1.3rem]">
+          <p className="text-gray-3 mt-5 mb-8 text-[.9rem] sm:text-[1rem] md:w-3/4 md:text-[1.1rem] lg:text-[1.1rem]">
             Upload your resume to get an instant ATS score and clear suggestions to improve it.
           </p>
-          <div>
+          <div className="text-left">
             <FileUploder />
           </div>
         </div>
