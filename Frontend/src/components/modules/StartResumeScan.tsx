13a45b4134df2a93b060c9d1cd1bff7d6e41219f
Ayush <ayushkarma.dev@gmail.com>
2025-12-24 16:11:54 +0530

    Change in typography in start resume scan section

--- Frontend/src/components/modules/StartResumeScan.tsx
+++ Frontend/src/components/modules/StartResumeScan.tsx
@@ -8,7 +8,7 @@ function StartResumeScan() {
         <h3 className="text-bg-gray-1 mt-4 text-xl font-medium md:text-3xl lg:text-4xl">
           Test Your Resume Against ATS Systems
         </h3>
-        <p className="text-gray-3 mt-5 mb-8 text-[.9rem] sm:text-[1rem] md:w-3/4 md:text-[1.1rem] lg:text-[1.1rem]">
+        <p className="text-gray-3 mt-5 mb-8 text-[.91rem] sm:text-[1rem] md:w-3/4 md:text-[1.1rem] lg:text-[1.1rem]">
           Upload your resume to get an instant ATS score and clear suggestions to improve it.
         </p>
         <div className="text-left">
